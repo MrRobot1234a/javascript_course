0205deb94471fd42f9b0abee15718a5220197b72
Akim <akimserverov75@gmail.com>
2023-07-07 17:45:04 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// alert('Hello');
+// const result  = confirm('Are you here&');
+// console.log(result);
+
+// const answer = +prompt('Сколько вам лет', '18');
+console.log(typeof(answer));
